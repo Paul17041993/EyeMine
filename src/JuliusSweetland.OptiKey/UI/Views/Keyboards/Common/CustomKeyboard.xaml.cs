310fca0a3bea1d54768154a716405e5a5181ff73
Kirsty McNaught <kirsty_mcn@yahoo.com>
2016-10-07 20:40:33 +0100

    WIP test custom keyboard

--- src/JuliusSweetland.OptiKey/UI/Views/Keyboards/Common/CustomKeyboard.xaml.cs
+++ src/JuliusSweetland.OptiKey/UI/Views/Keyboards/Common/CustomKeyboard.xaml.cs
@@ -34,20 +34,32 @@ namespace JuliusSweetland.OptiKey.UI.Views.Keyboards.Common
             newKey.Text = JuliusSweetland.OptiKey.Properties.Resources.BACK;
             newKey.Value = KeyValues.BackFromKeyboardKey;
 
-            MainGrid.Children.Add(newKey);
-            Grid.SetColumn(newKey, gridWidth - 1);
-            Grid.SetRow(newKey, gridHeight -1);
+            this.AddKey(newKey, gridHeight - 1, gridWidth - 1);
 
             Key newKey2 = new Key();
             newKey2.Text = "hello";
             newKey2.SharedSizeGroup = "KeyWithSymbolAndText";
-            newKey2.Value = new KeyValue("a");
+            newKey2.Value = new KeyValue("Hello, World");
 
-            MainGrid.Children.Add(newKey2);
-            Grid.SetColumn(newKey2, 1);
-            Grid.SetRow(newKey2, 1);
-            Grid.SetColumnSpan(newKey2, 2);
-            
+            this.AddKey(newKey2, 1, 1, 1, 2);
+
+            Key newKey3 = new Key();
+            newKey3.Text = "bye";
+            newKey3.SharedSizeGroup = "KeyWithSymbolAndText";
+            newKey3.Value = new KeyValue("Tara");
+
+            this.AddKey(newKey3, 0, 0);
+
+        }
+
+        private void AddKey(Key key, int row, int col, int rowspan = 1, int colspan = 1)
+        {
+            MainGrid.Children.Add(key);
+            Grid.SetColumn(key, col);
+            Grid.SetRow(key, row);
+            Grid.SetColumnSpan(key, colspan);
+            Grid.SetRowSpan(key, rowspan);
+
         }
     }
 }
